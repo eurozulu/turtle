fa460d5e7a9952122fe63f35d95823fc0e04c123
Rob Gilham <robert.gilham@linkpizza.com>
2021-04-05 17:09:28 +0200

    Added comment check at start of line. Now process each line at a time.

--- src/main/java/org/spoofer/interpreter/Interpreter.java
+++ src/main/java/org/spoofer/interpreter/Interpreter.java
@@ -14,6 +14,8 @@ import java.util.regex.Pattern;
  */
 public class Interpreter {
 
+    public static final String COMMENT_TOKEN = "#";
+
     private static final Logger LOG = Logger.getLogger(Interpreter.class.getName());
 
     // A map of the commands and their string name mappings.
@@ -49,21 +51,39 @@ public class Interpreter {
      * e.g. forward 10 rotate 90 forward 10 rotate -90 forward 20, is interpreted as three commands, each with a single argument.
      *
      * @param turtleState the state to apply the command(s) to.
-     * @param line        the line of command(s) to interpret.
+     * @param commands    the line of command(s) to interpret.
      * @throws IllegalArgumentException, Exception if commands, argument are invalid
      */
-    public void run(TurtleState turtleState, String line) throws Exception {
-        List<String> args = parseArgs(line);
-
+    public void run(TurtleState turtleState, String commands) throws Exception {
         State state = createState(turtleState);
-        while (!args.isEmpty()) {
-            Command command = parseCommand(args);
-            LOG.log(Level.INFO, String.format("\t'%s (%s)'\n", command.getClass().getSimpleName(), args.toString()));
 
-            command.execute(state, args);
+        for (String line : commands.split("\n")) {
+            if (line.trim().startsWith(COMMENT_TOKEN))
+                continue;
+
+            List<String> args = parseArgs(line);
+            while (!args.isEmpty()) {
+                Command command = parseCommand(args);
+                LOG.log(Level.FINE, String.format("read '%s' as:\n%s (%s)\n", commands, command.getClass().getSimpleName(), args.toString()));
+                command.execute(state, args);
+            }
         }
     }
 
+    /**
+     * parseCommand will remove the first argument and locate the corrisponding {@link Command} mathching it.
+     * If the command is not a known command mapping, the macro's are checked for the same command.
+     * When a matching macro is found the arguments are prepended with the macro command, merged into any arguments that marco defines.
+     * i.e. each and any argument tokens '{}' in the macro will remove a single argument from the args list to replace it.
+     * Any remaining arguments are then passed with the resulting macro, recurively into this method to restart the process of locating a command.
+     * If the macros do not contain the command, the same process as performed on macros, is applied to any matching pattern.
+     * <p>
+     * If no command, macro or pattern matches the command an IllegalArgumentException is thrown
+     *
+     * @param args the arguments to parse to find the next command.
+     *             Argument contents may be altered by this method, removing items for any required token replacements.
+     * @return The mapped, directly or indirectly (via patterns and macros), Command for the given arguments.
+     */
     private Command parseCommand(List<String> args) {
         String cmd = args.remove(0).toLowerCase();
 
@@ -72,13 +92,13 @@ public class Interpreter {
         }
 
         if (macros.containsKey(cmd)) {
-            List<String> macArgs = parseArgs(macros.get(cmd).toString(args));
+            List<String> macArgs = parseArgs(macros.get(cmd).Command(args));
             args.addAll(0, macArgs);
             return parseCommand(args);
         }
 
         if (patterns.containsKey(cmd)) {
-            List<String> patArgs = parseArgs(patterns.get(cmd).toString(args));
+            List<String> patArgs = parseArgs(patterns.get(cmd).Command(args));
             args.addAll(0, patArgs);
             return parseCommand(args);
         }
@@ -86,7 +106,7 @@ public class Interpreter {
     }
 
     /**
-     * parseLine parses the given line into a List of space seperated Strings, ignoring any spaces enclosed in quotes.
+     * parseLine parses the given line into a List of space separated Strings, ignoring any spaces enclosed in quotes.
      *
      * @param line line to parse
      * @return List of seperated strings
@@ -101,23 +121,26 @@ public class Interpreter {
         return list;
     }
 
-    // cleanCodeBlocks ensures delimiting [] brackets are spaced away from the enclosed commands so to act as an argument in themselves.
+    // cleanCodeBlocks ensures code-block delimiter [] brackets are spaced away from the surrounding commands,
+    // so they will act as an argument in themselves. (e.g. Common to write ']]' at the end of a nested block)
+    // ignores brackets inside double quoted strings.
     private String cleanCodeBlocks(String cmdline) {
         StringBuilder sb = new StringBuilder();
         boolean inQuotes = false;
-
         for (char ch : cmdline.toCharArray()) {
             if (ch == '\"') {
                 inQuotes = !inQuotes;
             }
-            String s = sb.toString();
-            boolean lastCharToken = s.endsWith("[") || s.endsWith("]");
-            boolean lastCharSpace = s.length() == 0 || Character.isWhitespace(s.charAt(s.length() - 1));
-            boolean thisCharToken = ch == ']' || ch == '[';
-
             if (!inQuotes) {
-                if ( (lastCharToken && !Character.isWhitespace(ch)) ||
-                        (thisCharToken && !lastCharSpace) ){
+                String s = sb.toString();
+                char lastChar = s.length() > 0 ? s.charAt(s.length() - 1) : 0;
+
+                boolean lastCharToken = lastChar == '[' || lastChar == ']';
+                boolean lastCharSpace = lastChar == 0 || Character.isWhitespace(lastChar);
+                boolean thisCharToken = ch == ']' || ch == '[';
+
+                if ((lastCharToken && !Character.isWhitespace(ch)) || // no following space
+                        (thisCharToken && !lastCharSpace)) {   // no leading space
                     sb.append(" ");
                 }
             }
@@ -180,43 +203,44 @@ public class Interpreter {
         commands.put("turtle", new TurtleVisibleCommand());
         commands.put("undo", new UndoCommand());
         commands.put("dump", new DumpCommand());
+        commands.put("run", new RunCommand());
     }
 
     // macros are a list of alternative text, which is replaced in the command line, with its mapped value.
     private static void buildMacros() {
         macros.clear();
-        macros.put("fd", new Macro("forward"));
-        macros.put("back", new Macro("forward -{}"));
-        macros.put("bk", new Macro("forward -{}"));
-        macros.put("right", new Macro("rotate"));
-        macros.put("rt", new Macro("rotate"));
-        macros.put("left", new Macro("rotate -{}"));
-        macros.put("lt", new Macro("rotate -{}"));
+        macros.put("fd", new SimpleMacro("forward"));
+        macros.put("back", new SimpleMacro("forward -{}"));
+        macros.put("bk", new SimpleMacro("forward -{}"));
+        macros.put("right", new SimpleMacro("rotate"));
+        macros.put("rt", new SimpleMacro("rotate"));
+        macros.put("left", new SimpleMacro("rotate -{}"));
+        macros.put("lt", new SimpleMacro("rotate -{}"));
 
-        macros.put("go", new Macro("forward"));
-        macros.put("turn", new Macro("rotate"));
-        macros.put("turn around", new Macro("rotate 180"));
-        macros.put("turn back", new Macro("rotate 180"));
+        macros.put("go", new SimpleMacro("forward"));
+        macros.put("turn", new SimpleMacro("rotate"));
+        macros.put("turn around", new SimpleMacro("rotate 180"));
+        macros.put("turn back", new SimpleMacro("rotate 180"));
 
-        macros.put("cleanscreen", new Macro("home clean"));
-        macros.put("cs", new Macro("cleanscreen"));
-        macros.put("hm", new Macro("home"));
-        macros.put("cn", new Macro("clean"));
+        macros.put("cleanscreen", new SimpleMacro("home clean"));
+        macros.put("cs", new SimpleMacro("cleanscreen"));
+        macros.put("hm", new SimpleMacro("home"));
+        macros.put("cn", new SimpleMacro("clean"));
 
-        macros.put("showturtle", new Macro("turtle true"));
-        macros.put("st", new Macro("turtle true"));
-        macros.put("hideturtle", new Macro("turtle false"));
-        macros.put("ht", new Macro("turtle false"));
+        macros.put("showturtle", new SimpleMacro("turtle true"));
+        macros.put("st", new SimpleMacro("turtle true"));
+        macros.put("hideturtle", new SimpleMacro("turtle false"));
+        macros.put("ht", new SimpleMacro("turtle false"));
 
-        macros.put("penup", new Macro("pen off"));
-        macros.put("pendown", new Macro("pen on"));
-        macros.put("pu", new Macro("pen off"));
-        macros.put("pd", new Macro("pen on"));
-        macros.put("draw", new Macro("pen on"));
+        macros.put("penup", new SimpleMacro("pen off"));
+        macros.put("pendown", new SimpleMacro("pen on"));
+        macros.put("pu", new SimpleMacro("pen off"));
+        macros.put("pd", new SimpleMacro("pen on"));
+        macros.put("draw", new SimpleMacro("pen on"));
 
-        macros.put("rp", new Macro("repeat"));
-        macros.put("pt", new Macro("pattern"));
-        macros.put("ud", new Macro("undo"));
+        macros.put("rp", new SimpleMacro("repeat"));
+        macros.put("pt", new SimpleMacro("pattern"));
+        macros.put("ud", new SimpleMacro("undo"));
     }
 
 
