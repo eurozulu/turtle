5e403d83c3bb9fe3f3e475273d804544a150fa2d
Rob Gilham <robert.gilham@linkpizza.com>
2021-04-05 17:12:20 +0200

    added run command to execute external logo file.

---
+++ src/main/java/org/spoofer/commands/RunCommand.java
@@ -0,0 +1,27 @@
+package org.spoofer.commands;
+
+import org.spoofer.interpreter.Interpreter;
+import org.spoofer.interpreter.State;
+import org.spoofer.misc.FileTools;
+import org.spoofer.model.TurtleState;
+
+import java.util.List;
+
+/**
+ * {@link RunCommand} will execute an external file with the file path given in the given argument.
+ */
+public class RunCommand implements Command {
+    @Override
+    public void execute(State state, List<String> args) throws Exception {
+        if (args.isEmpty()) {
+            throw new IllegalArgumentException("missing filename of logo file to run");
+        }
+
+        System.out.println(System.getProperty("user.dir"));
+
+        String cmdline = FileTools.readFile(args.remove(0));
+        Interpreter runtime = state.get("runtime");
+        TurtleState turtle = state.get("turtle");
+        runtime.run(turtle, cmdline);
+    }
+}
